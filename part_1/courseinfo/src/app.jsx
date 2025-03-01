45cd5b41357f6c2b0d9d7dde96ab82771f05192a
fercoopat <fercoopat950211@gmail.com>
2025-03-01 09:43:23 -0500

    part 1 - 1.5 exercise completed

--- part_1/courseinfo/src/app.jsx
+++ part_1/courseinfo/src/app.jsx
@@ -2,28 +2,30 @@ import Content from './components/content';
 import Header from './components/header';
 import Total from './components/total';
 
-const course = 'Half Stack application development';
-const parts = [
-  {
-    name: 'Fundamentals of React',
-    exercises: 10,
-  },
-  {
-    name: 'Using props to pass data',
-    exercises: 7,
-  },
-  {
-    name: 'State of a component',
-    exercises: 14,
-  },
-];
+const course = {
+  name: 'Half Stack application development',
+  parts: [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    },
+  ],
+};
 
 export default function App() {
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course?.parts} />
+      <Total parts={course?.parts} />
     </div>
   );
 }
