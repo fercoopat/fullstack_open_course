58ed50e4e2e5b1542cf975d7baf8e8fe0677fbbf
fercoopat <fercoopat950211@gmail.com>
2025-03-01 21:35:35 -0500

    part 2 - 2.13 exercise completed.

--- part_2/phonebook/src/api/persons.js
+++ part_2/phonebook/src/api/persons.js
@@ -1,13 +1,15 @@
 import { ApiClient } from './api-cilent';
 
+const RESOURCE = '/persons';
+
 export const getPersons = async () => {
-  const { data } = await ApiClient.get('/persons');
+  const { data } = await ApiClient.get(RESOURCE);
 
   return data;
 };
 
 export const addPerson = async (newPerson) => {
-  const { data } = await ApiClient.post('/persons', newPerson);
+  const { data } = await ApiClient.post(RESOURCE, newPerson);
 
   return data;
 };
