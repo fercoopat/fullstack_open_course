5b50e2c33fc7437e3d49e0ffbad75260de5b2ed5
fercoopat <fercoopat950211@gmail.com>
2025-03-08 13:15:04 -0500

    3.12 exercise completed.

--- part_3/phonebook/backend/src/constants/envs.mjs
+++ part_3/phonebook/backend/src/constants/envs.mjs
@@ -1,3 +1,5 @@
 import 'dotenv/config';
 
-export const PORT = +process.env.PORT || 3000;
+export const PORT = Number(process.env.PORT) || 3000;
+
+export const MONGO_URI = process.env.MONGO_URI || '';
