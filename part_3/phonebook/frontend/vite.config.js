e6cf2027a09e6d269c1816c3628a98dd0f1eecfd
fercoopat <fercoopat950211@gmail.com>
2025-03-08 11:40:59 -0500

    3.11 exercise updated.

--- part_3/phonebook/frontend/vite.config.js
+++ part_3/phonebook/frontend/vite.config.js
@@ -1,16 +1,26 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+  const env = loadEnv(mode, __dirname, '');
+
+  const target = env.VITE_API_BASE_URL || 'http://localhost:3000';
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  };
 });
