b8e434ee52837f0f7812f2883933c3bc33641ec0
fercoopat <fercoopat950211@gmail.com>
2025-03-09 23:52:18 -0400

    4.4 and 4.5 exercises completed.

--- part_4/blog_list/backend/utils/list-helper.js
+++ part_4/blog_list/backend/utils/list-helper.js
@@ -1,3 +1,20 @@
-export const dummy = (blogs) => {
+export const dummy = (blogs = []) => {
   return 1;
+};
+
+export const totalLikes = (blogs = []) => {
+  return blogs?.reduce((prev, curr) => prev + curr?.likes, 0);
+};
+
+export const favoriteBlog = (blogs = []) => {
+  if (!blogs?.length) {
+    return null;
+  }
+  return blogs?.reduce((prev, curr) => {
+    return prev?.likes > curr?.likes ? prev : curr;
+  }, blogs?.[0]);
+};
+
+export const sortBlogsByFav = (blogs = []) => {
+  return blogs?.sort((a, b) => b?.likes < a?.likes);
 };
