ff3076fba1a301cda96f9542584099b6cfadb8e0
fercoopat <fercoopat950211@gmail.com>
2025-03-13 19:09:26 -0400

    4.9 exercise completed.

--- part_4/blog_list/backend/models/blog.model.js
+++ part_4/blog_list/backend/models/blog.model.js
@@ -18,4 +18,12 @@ const blogSchema = new Schema({
   },
 });
 
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
 export const Blog = model('Blog', blogSchema);
--- part_4/blog_list/backend/tests/blog-api.test.js
+++ part_4/blog_list/backend/tests/blog-api.test.js
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose';
+import assert from 'node:assert';
 import { after, beforeEach, test } from 'node:test';
 import supertest from 'supertest';
 import app from '../app.js';
-import { Blog } from '../models/blog.model.js';
 import { DUMMY_BLOGS } from '../constants/dummy.blogs.js';
+import { Blog } from '../models/blog.model.js';
 
 const api = supertest(app);
 
@@ -21,6 +22,12 @@ test('blogs are returned as json', async () => {
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/);
+});
+
+test('blogs have an id property', async () => {
+  const response = await api.get('/api/blogs');
+
+  assert.ok(response.body[0].id, 'The blog post should have an `id` property');
 });
 
 after(async () => {
